9c38672d6caad2d63cf43bf511991d5287ff68f4
Olivier <olivier.github@gmail.com>
2013-01-20 21:02:58 +0800

    TestSCardService renamed DemoSCardService.
    Also support of 2 binding for the SCardService: NamedPipe and NetTCP

--- SCardServiceHost/MainForm.cs
+++ SCardServiceHost/MainForm.cs
@@ -19,148 +19,76 @@ namespace GemCard.Service.Host
     {
         const string 
             NET_TCP_BASE_ADDRESS = "net.tcp://localhost:8001/",
+            NET_NAMED_PIPE_BASE_ADDRESS = "net.pipe://localhost/",
             SERVCICE_ADDRESS = "SCardService";
 
-        private ServiceHost SCardServiceHost = null;
+        //private ServiceHostManager<IRemoteCard, RemoteCard> scardService = null;
+        private NamedPipeServiceHostManager<IRemoteCard, RemoteCard> scardService = null;
 
         public MainForm()
         {
             InitializeComponent();
 
+#if NET_TCP
+            scardService = new ServiceHostManager<IRemoteCard, RemoteCard>(NET_TCP_BASE_ADDRESS, SERVCICE_ADDRESS);
+#else
+            scardService = new NamedPipeServiceHostManager<IRemoteCard, RemoteCard>(NET_NAMED_PIPE_BASE_ADDRESS, SERVCICE_ADDRESS);
+#endif
+
+            scardService.Closed += ServiceHost_Closed;
+            scardService.Opened += ServiceHost_Opened;
+
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+        void ServiceHost_Opened(object sender, EventArgs e)
+        {
+            listBoxStatus.Items.Add("Smart card service started and running...");
+            btnStop.Enabled = true;
+            btnStart.Enabled = false;
+        }
+
+        void ServiceHost_Closed(object sender, EventArgs e)
+        {
+            listBoxStatus.Items.Add("Smart card service stopped.");
             btnStart.Enabled = true;
             btnStop.Enabled = false;
         }
 
         protected override void OnClosed(EventArgs e)
         {
-            StopService();
+            if (scardService != null)
+            {
+                scardService.Closed -= ServiceHost_Closed;
+                scardService.Opened -= ServiceHost_Opened;
+                scardService.Dispose();
+            }
             base.OnClosed(e);
         }
 
-        #region WCF service control
-        private bool StartService()
+        private void btnStart_Click(object sender, EventArgs e)
         {
-            bool started = false;
-
             try
             {
-                if (SCardServiceHost == null)
-                {
-                    SCardServiceHost = new ServiceHost(typeof(RemoteCard), new Uri(NET_TCP_BASE_ADDRESS));
-                    NetTcpBinding serviceBinding = new NetTcpBinding(SecurityMode.None);
-                    serviceBinding.TransactionFlow = true;
-                    SCardServiceHost.AddServiceEndpoint(typeof(IRemoteCard), serviceBinding, SERVCICE_ADDRESS);
-
-                    AddMetadataEchange(SCardServiceHost);
-                }
-
-                switch (SCardServiceHost.State)
-                {
-                    case CommunicationState.Closed:
-                        {
-                            SCardServiceHost = null;
-                            started = StartService();
-                            break;
-                        }
-
-                    case CommunicationState.Opened:
-                    case CommunicationState.Opening:
-                        {
-                            started = true;
-                            break;
-                        }
-
-                    case CommunicationState.Created:
-                        {
-                            SCardServiceHost.Open();
-                            listBoxStatus.Items.Add("Smart card service started and running...");
-                            started = true;
-                            break;
-                        }
-                }
-
-                SetButtonState();
+                scardService.Start();
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
-
-            return started;
-        }
-
-        private void AddMetadataEchange(ServiceHost host)
-        {
-            ServiceMetadataBehavior mBehave = new ServiceMetadataBehavior();
-            host.Description.Behaviors.Add(mBehave);
-
-            host.AddServiceEndpoint(typeof(IMetadataExchange),
-                MetadataExchangeBindings.CreateMexTcpBinding(),
-                Path.Combine(NET_TCP_BASE_ADDRESS, SERVCICE_ADDRESS, "mex"));
-        }
-
-        private void StopService()
-        {
-            if (SCardServiceHost != null)
-            {
-                switch (SCardServiceHost.State)
-                {
-                    case CommunicationState.Opened:
-                        {
-                            SCardServiceHost.Close();
-                            while (SCardServiceHost.State != CommunicationState.Closed)
-                            {
-                                // Wait!
-                            }
-                            listBoxStatus.Items.Add("Smart card service stopped");
-                            SCardServiceHost = null;
-                            break;
-                        }
-                }
-            }
-
-            SetButtonState();
-        }
-
-        private void SetButtonState()
-        {
-            if (SCardServiceHost != null)
-            {
-                switch (SCardServiceHost.State)
-                {
-                    case CommunicationState.Closed:
-                    case CommunicationState.Closing:
-                    case CommunicationState.Created:
-                        {
-                            btnStart.Enabled = true;
-                            break;
-                        }
-
-                    case CommunicationState.Opened:
-                    case CommunicationState.Opening:
-                        {
-                            btnStart.Enabled = false;
-                            break;
-                        }
-                }
-            }
-            else
-            {
-                btnStart.Enabled = true;
-            }
-
-            btnStop.Enabled = !btnStart.Enabled;
-        }
-        #endregion
-
-        private void btnStart_Click(object sender, EventArgs e)
-        {
-            StartService();
         }
 
         private void btnStop_Click(object sender, EventArgs e)
         {
-            StopService();
+            try
+            {
+                scardService.Stop();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btnClose_Click(object sender, EventArgs e)
---
+++ SCardServiceHost/ServiceHostManager.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+
+namespace GemCard.Service.Host
+{
+    /// <summary>
+    /// This class handles the lifecycle of a WCF service
+    /// </summary>
+    /// <typeparam name="I"></typeparam>
+    /// <typeparam name="T"></typeparam>
+    class TCPServiceHostManager<I, T> : IDisposable
+        where T : class
+        where I : class
+    {
+        private ServiceHost serviceHost = null;
+        private string baseAddress;
+        private string serviceAddress;
+
+        public EventHandler Opened;
+        public EventHandler Closed;
+
+        public TCPServiceHostManager(string baseAddress, string serviceAddress)
+        {
+            this.baseAddress = baseAddress;
+            this.serviceAddress = serviceAddress;
+        }
+
+        private void AddMetadataEchange(ServiceHost host, string baseAddress, string serviceAddress)
+        {
+            ServiceMetadataBehavior mBehave = new ServiceMetadataBehavior();
+            host.Description.Behaviors.Add(mBehave);
+
+            host.AddServiceEndpoint(typeof(IMetadataExchange),
+                MetadataExchangeBindings.CreateMexTcpBinding(),
+                Path.Combine(baseAddress, serviceAddress, "mex"));
+        }
+
+        public bool Start()
+        {
+            bool started = false;
+
+            if (serviceHost == null)
+            {
+                serviceHost = new ServiceHost(typeof(T), new Uri(baseAddress));
+                NetTcpBinding serviceBinding = new NetTcpBinding(SecurityMode.None);
+                serviceBinding.TransactionFlow = true;
+                serviceHost.AddServiceEndpoint(typeof(I), serviceBinding, serviceAddress);
+
+                // Attach event handlers
+                serviceHost.Opened += serviceHost_Opened;
+                serviceHost.Closed += serviceHost_Closed;
+
+                AddMetadataEchange(serviceHost, baseAddress, serviceAddress);
+            }
+
+            if (serviceHost != null)
+            {
+                switch (serviceHost.State)
+                {
+                    case CommunicationState.Closed:
+                        {
+                            serviceHost = null;
+                            started = Start();
+                            break;
+                        }
+
+                    case CommunicationState.Opened:
+                    case CommunicationState.Opening:
+                        {
+                            started = true;
+                            break;
+                        }
+
+                    case CommunicationState.Created:
+                        {
+                            serviceHost.Open();
+                            started = true;
+                            break;
+                        }
+                }
+            }
+
+            return started;
+        }
+
+        void serviceHost_Closed(object sender, EventArgs e)
+        {
+            if (Closed != null)
+            {
+                Closed(this, e);
+            }
+        }
+
+        void serviceHost_Opened(object sender, EventArgs e)
+        {
+            if (Opened != null)
+            {
+                Opened(this, e);
+            }
+        }
+
+        public void Stop()
+        {
+            if (serviceHost != null)
+            {
+                switch (serviceHost.State)
+                {
+                    case CommunicationState.Opened:
+                        {
+                            serviceHost.Close();
+                            while (serviceHost.State != CommunicationState.Closed)
+                            {
+                                // Wait!
+                            }
+                            serviceHost.Opened -= serviceHost_Opened;
+                            serviceHost.Closed -= serviceHost_Closed;
+                            serviceHost = null;
+                            break;
+                        }
+                }
+            }
+        }
+
+        public CommunicationState State
+        {
+            get
+            {
+                CommunicationState state = CommunicationState.Closed;
+
+                if (serviceHost != null)
+                {
+                    state = serviceHost.State;
+                }
+
+                return state;
+            }
+        }
+
+        public void Dispose()
+        {
+            Stop();
+        }
+    }
+
+    /// <summary>
+    /// This class handles the lifecycle of a WCF service
+    /// </summary>
+    /// <typeparam name="I"></typeparam>
+    /// <typeparam name="T"></typeparam>
+    class NamedPipeServiceHostManager<I, T> : IDisposable
+        where T : class
+        where I : class
+    {
+        private ServiceHost serviceHost = null;
+        private string baseAddress;
+        private string serviceAddress;
+
+        public EventHandler Opened;
+        public EventHandler Closed;
+
+        public NamedPipeServiceHostManager(string baseAddress, string serviceAddress)
+        {
+            this.baseAddress = baseAddress;
+            this.serviceAddress = serviceAddress;
+        }
+
+        private void AddMetadataEchange(ServiceHost host, string baseAddress, string serviceAddress)
+        {
+            ServiceMetadataBehavior mBehave = new ServiceMetadataBehavior();
+            host.Description.Behaviors.Add(mBehave);
+
+            host.AddServiceEndpoint(typeof(IMetadataExchange),
+                MetadataExchangeBindings.CreateMexNamedPipeBinding(),
+                Path.Combine(baseAddress, serviceAddress, "mex"));
+        }
+
+        public bool Start()
+        {
+            bool started = false;
+
+            if (serviceHost == null)
+            {
+                serviceHost = new ServiceHost(typeof(T), new Uri(baseAddress));
+                NetNamedPipeBinding serviceBinding = new NetNamedPipeBinding();
+                serviceBinding.TransactionFlow = true;
+                serviceHost.AddServiceEndpoint(typeof(I), serviceBinding, serviceAddress);
+
+                // Attach event handlers
+                serviceHost.Opened += serviceHost_Opened;
+                serviceHost.Closed += serviceHost_Closed;
+
+                AddMetadataEchange(serviceHost, baseAddress, serviceAddress);
+            }
+
+            if (serviceHost != null)
+            {
+                switch (serviceHost.State)
+                {
+                    case CommunicationState.Closed:
+                        {
+                            serviceHost = null;
+                            started = Start();
+                            break;
+                        }
+
+                    case CommunicationState.Opened:
+                    case CommunicationState.Opening:
+                        {
+                            started = true;
+                            break;
+                        }
+
+                    case CommunicationState.Created:
+                        {
+                            serviceHost.Open();
+                            started = true;
+                            break;
+                        }
+                }
+            }
+
+            return started;
+        }
+
+        void serviceHost_Closed(object sender, EventArgs e)
+        {
+            if (Closed != null)
+            {
+                Closed(this, e);
+            }
+        }
+
+        void serviceHost_Opened(object sender, EventArgs e)
+        {
+            if (Opened != null)
+            {
+                Opened(this, e);
+            }
+        }
+
+        public void Stop()
+        {
+            if (serviceHost != null)
+            {
+                switch (serviceHost.State)
+                {
+                    case CommunicationState.Opened:
+                        {
+                            serviceHost.Close();
+                            while (serviceHost.State != CommunicationState.Closed)
+                            {
+                                // Wait!
+                            }
+                            serviceHost.Opened -= serviceHost_Opened;
+                            serviceHost.Closed -= serviceHost_Closed;
+                            serviceHost = null;
+                            break;
+                        }
+                }
+            }
+        }
+
+        public CommunicationState State
+        {
+            get
+            {
+                CommunicationState state = CommunicationState.Closed;
+
+                if (serviceHost != null)
+                {
+                    state = serviceHost.State;
+                }
+
+                return state;
+            }
+        }
+
+        public void Dispose()
+        {
+            Stop();
+        }
+    }
+}
