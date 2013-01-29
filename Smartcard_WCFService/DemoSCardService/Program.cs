c50184a33f0248d620f6340ad79a605e957a4b93
Olivier <olivier.github@gmail.com>
2013-01-29 22:46:03 +0800

    Adding CPL comment

--- Smartcard_WCFService/DemoSCardService/Program.cs
+++ Smartcard_WCFService/DemoSCardService/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/**
+ * @author Olivier ROUIT
+ * 
+ * @license CPL, CodeProject license 
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
